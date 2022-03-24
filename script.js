e77850099b83516a98ce91bc9562d06fa44b10a7
HunterQ <87006322+HunterWQuinn@users.noreply.github.com>
2022-03-23 20:49:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal === ""){
+        alert("Enter a positive number");
+
+    } else{
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100; 
+    var year = new Date().getFullYear()+parseInt(years);
+    var final = "If you deposit " + principal + ", at an interest rate of " + rate + "% You will receive an amount of " + interest + ", in the year " + year;
+    document.getElementById("result").innerText= final;
+ }
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}  
